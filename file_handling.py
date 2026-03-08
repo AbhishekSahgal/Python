4a1718bb0d752c58028acfc4f0b248244f2b3b8d
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-08 20:58:45 +0530

    file handling2

--- file_handling.py
+++ file_handling.py
@@ -43,3 +43,74 @@
 
 # Solution 
 
+# with open("hello.txt", 'r') as file:
+#     content = file.read()
+
+# print(content)
+
+
+# Q3
+
+# Append this line to the file:
+# Learning Python is fun
+
+# Solution 
+
+# with open("hello.txt", 'a') as file:
+#     file.write(" Learning Python is fun")
+
+
+# Q4
+
+# Create a program that writes 5 numbers into a file.
+
+# Example:
+
+# 10
+# 20
+# 30
+# 40
+# 50
+
+# with open("hello.txt", 'w') as file:
+#     for i in range(1,6):
+#         file.write(str(10*i) + "\n")
+        
+
+# Q6
+# Read a file and print each line using a loop.
+
+# with open("hello.txt", 'r') as file:
+#     for i in file:
+#         print(i.split())
+
+
+# Q7
+
+# Store this list into a file:
+
+# ["Apple", "Banana", "Mango"]
+
+# Each item should be written on a new line.
+
+# Solution 
+
+# list = ["Apple", "Banana", "Mango"]
+
+# with open("hello.txt", 'w') as file:
+#     for i in list:
+#         file.write(i + "\n")
+
+
+# Practice — Counting Lines
+# Q8
+
+# Write a program that counts how many lines exist in a file.
+
+count = 0
+
+with open("hello.txt", 'r') as file:
+    for i in file:
+        count = count + 1
+        file.read()
+

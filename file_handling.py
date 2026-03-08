c68000fbe0d396c751f6cfe0a648d77287173698
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-08 20:20:44 +0530

    file handling

---
+++ file_handling.py
@@ -0,0 +1,45 @@
+# file = open("data.txt", "r")
+
+# content = file.read()
+
+# print(content)
+# file.close
+
+
+#  Reading Line by Line
+
+# with open("data.txt", "r") as file:
+#     for line in file:
+#         print(line)
+
+
+# 4️⃣ Reading Line by Line
+
+# with open('data.txt', 'w') as file:
+#     file.write(" Hello Abhishek sahgal, What are you doing curretly") 
+#     file.write("\n Hello Abhishek sahgal,") 
+
+# import os
+
+# if os.path.exists("datamodel.txt"):
+#     print("file exists")
+# else:
+#     print("File does not exist")
+
+
+
+# Create a program that writes:
+# Hello Python
+# into a file named hello.txt.
+
+# Solution
+
+# with open("hello.txt", 'w') as file:
+#     file.write("Hello Python")
+
+# Q2
+
+# Write a program that reads and prints the contents of hello.txt.
+
+# Solution 
+

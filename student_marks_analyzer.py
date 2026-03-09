a4ef3a553d0773b3b65e6351f2a2b43c1509b48e
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-09 23:55:32 +0530

    list dictionary

--- student_marks_analyzer.py
+++ student_marks_analyzer.py
@@ -17,12 +17,40 @@
 
 #  Solution
 
+
 students = [
-    {"name":"Abhay","marks":90},
-    {"name":"Rahul","marks":70},
-    {"name":"Neha","marks":85}
+    {
+        "name":"Abhay",
+        "marks":90
+     },
+    {
+        "name":"Rahul",
+        "marks":70
+    },
+    {
+    "name":"Neha",
+    "marks":95
+    }
 ]
 
+top_student = students[0]
 
-for key, values in enumerate(students):
-    
+for student in students:
+    if student['marks'] > top_student['marks']:
+        top_student = student
+
+print("Top student nme is: ",top_student['name'])
+
+total = 0
+
+for student in students:
+    total = total + student['marks']
+
+result = total/len(student)
+
+print("Average is: ",result)
+
+print("All students name is")
+
+for student in students:
+    print(student['name'])

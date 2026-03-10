f726d951d95abede5d89faff22612d487a929a78
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-10 20:42:37 +0530

    list dictionary

---
+++ 13_list_comprehension.py
@@ -0,0 +1,14 @@
+# Task 1
+# Create list:
+# numbers = [1,2,3,4,5]
+# Using list comprehension create:
+# [1,4,9,16,25]
+
+# Solution
+
+numbers = [1,2,3,4,5]
+
+
+result = [n*n for n in numbers]
+print(result)
+

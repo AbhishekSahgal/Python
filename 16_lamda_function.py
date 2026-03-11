8ab4f0ef23d501c7f65a2a91456e1f39178c36b7
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-11 23:25:25 +0530

    lamda func

--- 16_lamda_function.py
+++ 16_lamda_function.py
@@ -2,7 +2,10 @@ square = lambda x: x*x
 
 print(square(10))
 
-
 add = lambda a,b : a+b 
 
 print(add(10,20))
+
+
+
+
---
+++ 17_advanced_strings.py
@@ -0,0 +1,42 @@
+# Advanced strings 
+
+# name = input("Enter your name: ")
+
+# print(name.casefold())
+
+
+# sentence = "Python is very powerful"
+
+# words = sentence.split()
+
+# print(words)
+
+
+# sentance2 = ' '.join(words)
+
+# print(sentance2)
+
+
+# text = "I am a java deverloper"
+
+# print(text.replace("java","python"))
+
+# name = "Abhishek Sahagl"
+# age = 21
+
+# print(f"My name is {name} and i am {age} old")
+
+
+# Take input sentence and print number of words.
+# Example:
+# Input: Python is very powerful
+# Output: 4
+# Hint:
+# split()
+# len()
+
+sentance = input("Enter your sentance: ")
+
+words = sentance.split()
+
+print(len(words))

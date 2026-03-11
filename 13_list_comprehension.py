6e929be271f93d264ec25bfd83f0171b1609764b
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-11 15:28:12 +0530

    list dictionary

--- 13_list_comprehension.py
+++ 13_list_comprehension.py
@@ -6,9 +6,19 @@
 
 # Solution
 
-numbers = [1,2,3,4,5]
+# numbers = [1,2,3,4,5]
 
 
-result = [n*n for n in numbers]
-print(result)
+# result = [n*n for n in numbers]
+# print(result)
+
+
+# Task 2
+# From list:
+# numbers = [10,15,20,25,30]
+# Create list of numbers divisible by 10.
+# Expected output:
+# [10,20,30]
+
+# Solution
 

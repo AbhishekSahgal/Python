301f2643dd3922dabdb9753513574ef46afe2690
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-14 14:47:35 +0530

    classes

---
+++ 19_oops.py
@@ -0,0 +1,37 @@
+# creating my first class 
+
+# class student:
+#     name = "Abhishek"
+#     marks = 10
+
+# s1 = student()
+
+# print(s1.name)
+# print(s1.marks)
+
+
+# class student:
+#     def __init__(self,name,marks):
+#         self.name = name
+#         self.marks = marks
+
+
+# Constructor (__init__) – Very Important
+# Used to initialize object data.
+
+# class student:
+#     def __init__(self,name,marks):
+#         self.name = name
+#         self.marks = marks
+
+# s1 = student("Abhay",100)
+
+# print(s1.name)
+# print(s1.marks)
+
+# s2 = student("Abhishek",90)
+
+# print(s2.name)
+# print(s2.marks)
+        
+

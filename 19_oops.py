5e0ee2633449e5219ea286b15a5918d5a02c0adf
abhishek sahgal <ticckusahgal123@gmail.com>
2026-03-15 11:27:32 +0530

    classes and methods

--- 19_oops.py
+++ 19_oops.py
@@ -35,3 +35,55 @@
 # print(s2.marks)
         
 
+
+# class student:
+#     def __init__(self,name, marks):
+#         self.name = name
+#         self.marks = marks
+    
+#     def show(self):
+#         print("The student name is",self.name,"and the marks are",self.marks)
+
+
+# s1 = student("Abhishek Sahgal", 95)
+# s2 = student("Abhay Sahgal", 85)
+
+# s1.show()
+# s2.show()
+
+
+# Create class Car with:
+# brand
+# speed
+# Create object and print details.
+
+# solution
+
+# class car:
+#     def __init__(self,brand,speed):
+#         self.brand = brand
+#         self.speed = speed
+    
+#     def show(self):
+#         print("Car brand is", self.brand, "and its top speed is", self.speed,"km/h")
+
+# c1 = car("BMW",380)
+# c2 = car("Audi",250)
+
+# c1.show()
+# c2.show()
+
+
+# 🚀 Mini Project
+# Student Result System (OOP Version)
+# Class should:
+# take name
+# take marks
+# method → print Pass / Fail
+# Rule:
+# marks >= 40 → Pass
+# else → Fail
+
+
+# Solution
+
